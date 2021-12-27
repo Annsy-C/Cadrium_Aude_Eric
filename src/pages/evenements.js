7eeaa467af6942f438b2653a841749db7223f038
AS Chollet <annesylvie.chollet@gmail.com>
2021-12-27 17:19:49 +0100

    nouvel horaire

--- src/pages/evenements.js
+++ src/pages/evenements.js
@@ -1,20 +1,12 @@
 import React from 'react';
 import EvenementBox from '../elements/evenement_box';
 import vitrine from'../images/Devanture_2.jpg';
-import interieur from'../images/Interieur.jpg';
 
 const Evenements = () => (
 	<section className="section">
 		<EvenementBox 
-			title="Ouverture Exceptionnelle"
-			description1="Pour le nouvel an, votre magasin sera exceptionnellement ouvert le vendredi 31 décembre de 10h à 15h."
-			description2=""
-			src={interieur}
-			alt="vitrine"
-		/>
-		<EvenementBox 
 			title="Fermeture Exceptionnelle"
-			description1="Votre magasin fermera ses portes exceptionnellement le mardi 4 janvier 2022."
+			description1="Votre magasin fermera ses portes exceptionnellement le  vendredi 31 décembre 2021 et le mardi 4 janvier 2022."
 			description2="Réouverture prévue le mercredi 5 janvier 2022 à 10h."
 			src={vitrine}
 			alt="vitrine de noël"
