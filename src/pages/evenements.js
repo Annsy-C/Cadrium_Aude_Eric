a8fe0ae88e832c9feafc015d59afc1bb2cde7bab
AS Chollet <annesylvie.chollet@gmail.com>
2022-12-14 17:30:34 +0100

    feat ajout evenement fin d'annee

--- src/pages/evenements.js
+++ src/pages/evenements.js
@@ -1,33 +1,23 @@
 import React from 'react';
 import EvenementBox from '../elements/evenement_box';
-import Box, { BoxContent, BoxImage } from "../elements/box";
-import Card, { CardImage } from "../elements/card";
-//import vitrine from'../images/Devanture_2.jpg';
-import halloween1 from '../images/evenements/halloween1.jpg';
-import halloween2 from '../images/evenements/halloween2.jpg';
-import halloween3 from '../images/evenements/halloween3.jpg';
+//import Box, { BoxContent, BoxImage } from "../elements/box";
+//import Card, { CardImage } from "../elements/card";
+import vitrine from'../images/Devanture_2.jpg';
+//import halloween1 from '../images/evenements/halloween1.jpg';
+//import halloween2 from '../images/evenements/halloween2.jpg';
+//import halloween3 from '../images/evenements/halloween3.jpg';
 
 const Evenements = () => (
 	<section className="section">
 		<div className="columns">
 			<div className="column">
-				<Card>
-					<CardImage src={halloween2} alt="citrouille" size="" />
-				</Card>
-			</div>
-			<div className="column is-half">
 				<EvenementBox
-					title="Halloween"
-					description1="Halloween s'invite chez Cadrium. Venez découvrir des citrouilles velours colorées pour égayer votre intérieur !"
-					description2=""
-					src={halloween1}
-					alt="citrouilles"
+					title="Fêtes de fin d'année"
+					description1="Pour les fêtes, ouverture exceptionnelle de votre boutique le dimanche 18 décembre de 10h à 18h et le lundi 19 décembre de 14h à 18h."
+					description2="Fermeture exceptionnelle pour congés à partir du 31 décembre au soir, réouverture prévue le mardi 10 janvier."
+					src={vitrine}
+					alt="vitrine"
 				/>
-			</div>
-			<div className="column">
-				<Card>
-					<CardImage src={halloween3} alt="citrouille" size="" />
-				</Card>
 			</div>
 		</div>
 
