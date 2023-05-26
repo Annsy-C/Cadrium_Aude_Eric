eba5a6e7ab068f41c9238ae10a44d4e4d17ae5ac
AS Chollet <annesylvie.chollet@gmail.com>
2023-05-26 12:15:19 +0200

    ajout evenement foire de chateaurouw

--- src/pages/evenements.js
+++ src/pages/evenements.js
@@ -7,9 +7,21 @@ import vitrine from'../images/Devanture_2.jpg';
 //import halloween2 from '../images/evenements/halloween2.jpg';
 //import halloween3 from '../images/evenements/halloween3.jpg';
 //import noel from '../images/evenements/Noel.jpg';
+import festival from '../images/evenements/festival.jpg';
 
 const Evenements = () => (
 	<section className="section">
+		<div className="columns">
+			<div className="column">
+				<EvenementBox
+					title="Foire de Chateauroux 2023"
+					description1="Retrouvez nous à notre stand à la foire exposition de Chateauroux (hall 2) du mercredi 24 au lundi 29 mai ! Réductions sur une sélection de produits."
+					description2="Votre magasin reste ouvert aux horaires habituels pendant la durée de la foire."
+					src={festival}
+					alt="festival"
+				/>
+			</div>
+		</div>
 		<div className="columns">
 			<div className="column">
 				<EvenementBox
@@ -21,7 +33,6 @@ const Evenements = () => (
 				/>
 			</div>
 		</div>
-
 	</section>
 );
 
