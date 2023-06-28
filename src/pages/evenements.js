bdea5c750f24b58c55edafd82520202defea9b73
AS Chollet <annesylvie.chollet@gmail.com>
2023-06-28 10:37:53 +0200

    horaires été

--- src/pages/evenements.js
+++ src/pages/evenements.js
@@ -2,23 +2,24 @@ import React from 'react';
 import EvenementBox from '../elements/evenement_box';
 //import Box, { BoxContent, BoxImage } from "../elements/box";
 //import Card, { CardImage } from "../elements/card";
-import vitrine from'../images/Devanture_2.jpg';
+import vitrine1 from'../images/Devanture_1.jpg';
+import vitrine2 from'../images/Devanture_2.jpg';
 //import halloween1 from '../images/evenements/halloween1.jpg';
 //import halloween2 from '../images/evenements/halloween2.jpg';
 //import halloween3 from '../images/evenements/halloween3.jpg';
 //import noel from '../images/evenements/Noel.jpg';
-import festival from '../images/evenements/festival.jpg';
+//import festival from '../images/evenements/festival.jpg';
 
 const Evenements = () => (
 	<section className="section">
 		<div className="columns">
 			<div className="column">
 				<EvenementBox
-					title="Foire de Chateauroux 2023"
-					description1="Retrouvez nous à notre stand à la foire exposition de Chateauroux (hall 2) du mercredi 24 au lundi 29 mai ! Réductions sur une sélection de produits."
-					description2="Votre magasin reste ouvert aux horaires habituels pendant la durée de la foire."
-					src={festival}
-					alt="festival"
+					title="Horaires d'été du 11 juillet au 20 août 2023"
+					description1="Mardi, mercredi,vendredi de 10h à 12h et de 14h à 18h. Samedi de 10h à 18h30. Fermeture les lundi, jeudi et dimanche."
+					description2="Congés annuels : fermeture du mardi 4 au samedi 8 juillet inclus. Réouverture mardi 11 juillet."
+					src={vitrine2}
+					alt="vitrine"
 				/>
 			</div>
 		</div>
@@ -28,7 +29,7 @@ const Evenements = () => (
 					title="Nouveaux horaires à compter du 1er février 2023"
 					description1="Du mardi au vendredi : 10h-12h / 14h-18h. Le samedi : 10h-18h30"
 					description2="Fermeture le dimanche et le lundi"
-					src={vitrine}
+					src={vitrine1}
 					alt="vitrine"
 				/>
 			</div>
